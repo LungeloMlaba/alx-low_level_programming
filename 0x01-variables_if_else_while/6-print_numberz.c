9998757b7a9ddbf417c4af24630689e91af6d6c4
Lungelo Mlaba <lungelomlaba49@gmail.com>
2023-06-18 06:56:22 -0700

    print

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints numbers from base 10.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+        int q;
+
+        q = 0;
+        while
+        (q < 10) {
+                putchar(q + '0');
+                        q++;
+        }
+putchar('\n');
+return (0);
+}

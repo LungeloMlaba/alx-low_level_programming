a38bf8414e32febb2cbe116ea879905f6ae60ca1
Lungelo Mlaba <lungelomlaba49@gmail.com>
2023-06-18 07:28:45 -0700

    numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char q;
+	int s;
+
+	q = 'a';
+	s = '0';
+
+	while (s < 10)
+	{
+		putchar(s + '0');
+		s++;
+	}
+	while (q <= 'f')
+	{
+		putchar(q);
+		q++;
+	}
+	putchar('\n');
+	return (0);
+}

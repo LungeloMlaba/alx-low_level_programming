04665e47033ebcf20f9eba0931230aa7b6e571ae
Lungelo Mlaba <lungelomlaba49@gmail.com>
2023-06-18 04:46:31 -0700

    las

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,19 +17,13 @@ int main(void)
 	x = n % 10;
 
 
-	if (n > 5)
-	{
+	if (	x > 5)
 		printf("Last digit of %d is %d and is greator than 5\n", n, x);
-	}
-	else if (n == 0)
-	{
+	
+	if (x == 0)
 		printf("Last digit of %d is %d and is 0\n", n, x);
-	}
-	else
-	{
+	if (x < 6 && x != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
-	}
-
-
+	
 	return (0);
 }

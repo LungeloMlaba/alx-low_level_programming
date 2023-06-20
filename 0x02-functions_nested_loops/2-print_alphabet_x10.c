b7770414162edc552b35115fa25f09abb71dafee
Lungelo Mlaba <lungelomlaba49@gmail.com>
2023-06-20 11:39:58 -0700

    functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints alphabet in lowercase
+ * print_alphabet - make alphabet x10 times
  */
 void print_alphabet_x10(void)
 {
@@ -9,13 +9,13 @@ void print_alphabet_x10(void)
 	int i = 0;
 
 	while (i <= 9)
-	{
+
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 
 			_putchar('\n');
 			i++;
-		}
+		
 	}
 }

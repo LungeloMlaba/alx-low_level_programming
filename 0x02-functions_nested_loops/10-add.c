7525b25b1f97a8127faac6ed6bc1668742d7d803
Lungelo Mlaba <lungelomlaba49@gmail.com>
2023-06-21 03:04:39 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @q: First integer
+ * @w: Second integer
+ * Return: The sum of both integers
+ */
+int add(int q, int w)
+{
+	return (q + w);
+}

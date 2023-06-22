0f611a56935a7a1d77194354b0881640a8fae05f
Lungelo Mlaba <lungelomlaba49@gmail.com>
2023-06-22 07:38:27 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks wheather letter is in uppercase
+ * @q: The num to be checked
+ * Return: 1 for upper letter and 0 for anything else
+ */
+int _isupper(int q)
+{
+	if (q >= 65 && q <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}

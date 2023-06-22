7de8eff6f795507bc3d69198cfbd5a5183f1739a
Lungelo Mlaba <lungelomlaba49@gmail.com>
2023-06-22 08:10:43 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _mul - Multiplies two integers
+ * @q: The first num to be multiplied
+ * @w: The second num to be multiplied
+ * Return: The value of multiplication of int q and int w
+ */
+int _mul(int q, int w)
+{
+	return (q + w);
+}

e859c7052640a56716fd5cd57528a89949be4ad6
Lungelo Mlaba <lungelomlaba49@gmail.com>
2023-06-22 08:26:37 -0700

    fuctions

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _mul - Multiplies two integers
+ * mul - Multiplies two integers
  * @q: The first num to be multiplied
  * @w: The second num to be multiplied with first num
  * Return: The result of multiplication of parameter q and parameter w
  */
-int _mul(int q, int w)
+int mul(int q, int w)
 {
 	return (q + w);
 }

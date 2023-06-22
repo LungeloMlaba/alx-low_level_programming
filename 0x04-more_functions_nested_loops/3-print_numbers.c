931ad5fb22dca4b3a0c5b82285deefde3dfee849
Lungelo Mlaba <lungelomlaba49@gmail.com>
2023-06-22 08:40:33 -0700

    dd

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Return: Numbers from 0 to 9
+ */
+
+int print_numbers(void)
+{
+        int q;
+
+        for (q = 0; q <= 9; q++)
+        {
+                _putchar(x + '0');
+        }
+        _putchar('\n');
+}

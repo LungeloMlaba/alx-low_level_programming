4cb975a9d1c9b328fbe6d6e007686ef837c342a9
Lungelo Mlaba <lungelomlaba49@gmail.com>
2023-06-26 05:10:44 -0700

    dd

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * prints_rev - prints a string in reverse
+ * @s: the string to print
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int r = 0;
+
+	while (s[r] != '\0')
+	{
+		r++;
+	}
+	for (r -=1; r >= 0; r--)
+	{
+		_putchar(s[r]);
+	}
+	_putchar('\n');
+}

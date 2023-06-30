ee03ffc7c3f8c562154880f559e525285a036a75
Lungelo Mlaba <lungelomlaba49@gmail.com>
2023-06-30 09:01:59 -0700

    dd

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,16 +9,17 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0, src_len = 0;
-
-	while (src[index++])
-	{	src_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-			dest[index] = src[index];
-
-			for (index = src_len; index < n; index++)
-			dest[index] = '\0';
+	int b;
+	b = 0;
+	while (b < n && src[b] != '\0')
+	{
+		dest[b] = src[b];
+		b++;
 	}
-			return (dest);
+	while (b < n)
+	{
+		dest[b] = '\0';
+		b++;
+	}
+	return (dest);
 }

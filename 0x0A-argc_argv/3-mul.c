e1eedc2e03862a7414e1f77764a815769432e5ad
LungeloMlaba <lungelomlaba49@gmail.com>
2023-10-03 09:07:54 -0700

    qq

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
-#include  "main.h"
+#include "main.h"
 
 /**
- * atoi - converts a string to na int
- * @s: string to be converted
+ * _atoi - converts string to integer
+ * @s: string to convert
  *
- * Return: the int converted from string
+ * Return: Always 0 (success)
  */
+
 int _atoi(char *s)
 {
-	int a, d, n, len, f, digit;
+	int i, d, n, len, f, digit;
 
-	a = 0;
+	i = 0;
 	d = 0;
 	n = 0;
 	len = 0;
@@ -20,19 +21,49 @@ int _atoi(char *s)
 
 	while (s[len] != '\0')
 		len++;
-
-	while(a < len && f == 0)
+	while (i < len && f == 0)
 	{
-		if (s[a] == '_')
+		if (s[i] == '-')
 			++d;
 
-
-		if (s[a] >= '0' && s[a] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			digit = s[a] - '0';
+			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-			if (s[a + d] == '0')
-				n++;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+		return (0);
+	return (n);
+}
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: On success 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+	return (0);
+}
